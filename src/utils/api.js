1a1cdb88eb9f498a78282dd78a44328a83c09bc2
srivarshiniR <113798278+srivarshiniR@users.noreply.github.com>
2025-02-07 02:49:19 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setAuthToken, clearAuthToken } from '../utils/auth';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://shopease-api-w4vt.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
